e866ec9af3fccba0ff9223d13e0278527234b872
DanieLuque <Danielcaluque502@gmail.com>
2026-06-04 16:59:51 -0500

    subo ejercicio 3 flutter.

--- ejercicio3/lib/main.dart
+++ ejercicio3/lib/main.dart
@@ -1,121 +1,329 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Biblioteca Personal',
+      home: Eventos_academicos(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Eventos_academicos extends StatelessWidget {
+  const Eventos_academicos({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void eventos(BuildContext context, Widget pantalla) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => pantalla));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Eventos Académicos'),
+        backgroundColor: const Color.fromARGB(255, 8, 185, 38),
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Icon(
+              Icons.event,
+              size: 100,
+              color: const Color.fromARGB(255, 8, 185, 38),
+            ),
+            const SizedBox(height: 20),
+            Text('Bienvenido a la sección de eventos'),
+            Card(
+              child: ListTile(
+                leading: Icon(
+                  Icons.calendar_today,
+                  color: const Color.fromARGB(255, 8, 185, 38),
+                ),
+                title: Text('lista dinamica'),
+                subtitle: Text('Eventos'),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  eventos(context, const ListaDinamica());
+                },
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(
+                  Icons.calendar_today,
+                  color: const Color.fromARGB(255, 8, 185, 38),
+                ),
+                title: Text('detalles de evento'),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  eventos(context, const DetallesEventos());
+                },
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(
+                  Icons.calendar_today,
+                  color: const Color.fromARGB(255, 8, 185, 38),
+                ),
+                title: Text('Formulario de Inscripción'),
+                subtitle: Text('Hora del evento'),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  eventos(context, const Formularios());
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class ListaDinamica extends StatelessWidget {
+  const ListaDinamica({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Eventos Académicos'),
+        backgroundColor: const Color.fromARGB(255, 5, 112, 23),
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text('Bienvenido a la sección de eventos'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.event,
+                        color: const Color.fromARGB(255, 5, 112, 23),
+                      ),
+                      title: Text('Evento ${index + 1}'),
+                      subtitle: Text('Descripción del evento ${index + 1}'),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const DetallesEventos(),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Regresar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetallesEventos extends StatelessWidget {
+  const DetallesEventos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Detalle del Evento')),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Card(
+          child: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.app_registration,
+                    color: Color.fromARGB(255, 104, 180, 53),
+                  ),
+                  title: Text('Feria Tecnológica'),
+                  subtitle: Text(
+                    'Fecha: 15/06/2026\n'
+                    'Lugar: Auditorio Principal\n'
+                    'Duración: 4 horas\n'
+                    'Tipo: Conferencia',
+                  ),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.app_registration,
+                    color: Color.fromARGB(255, 104, 180, 53),
+                  ),
+                  title: Text('Feria Mental'),
+                  subtitle: Text(
+                    'Fecha: 15/06/2026\n'
+                    'Lugar: Auditorio Principal\n'
+                    'Duración: 4 horas\n'
+                    'Tipo: Conferencia',
+                  ),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.app_registration,
+                    color: Color.fromARGB(255, 104, 180, 53),
+                  ),
+                  title: Text('Feria Mental'),
+                  subtitle: Text(
+                    'Fecha: 15/06/2026\n'
+                    'Lugar: Auditorio Principal\n'
+                    'Duración: 4 horas\n'
+                    'Tipo: Conferencia',
+                  ),
+                ),
+              ),
+
+              ElevatedButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Regresar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class Formularios extends StatefulWidget {
+  const Formularios({super.key});
+
+  @override
+  State<Formularios> createState() => _FormulariosState();
+}
+
+class _FormulariosState extends State<Formularios> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Formulario de Inscripción'),
+        backgroundColor: const Color.fromARGB(255, 8, 185, 38),
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Nombre del participante',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Ingrese su nombre';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Correo',
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          !value.contains('@') ||
+                          !value.contains('.')) {
+                        return 'Ingrese un correo válido';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Documento',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.length < 6) {
+                        return 'Mínimo 6 caracteres';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Programa de formación',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Ingrese el programa';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Inscripción realizada'),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Inscribirse'),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Regresar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
